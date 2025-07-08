57b24a6e6163295c3a08c86d07b2f3939615f41f
Eduard Kakosyan <kakosyaneduard@dal.ca>
2025-07-08 18:33:11 -0300

    fixing to build on my phone as an app

---
+++ FinSync/test-metro.js
@@ -0,0 +1,5 @@
+// Simple test file to check Metro bundler
+console.log('Metro bundler test - this should serve as application/javascript');
+export default function TestComponent() {
+  return null;
+}
