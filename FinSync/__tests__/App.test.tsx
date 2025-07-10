40dd90c15d164516947265b5e507fbe2675bf6ff
Eduard Kakosyan <kakosyaneduard@dal.ca>
2025-07-10 09:09:08 -0300

    reset to clean state

--- FinSync/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import RootLayout from "../app/_layout";
-
-describe("App", () => {
-  it("renders correctly", () => {
-    render(<RootLayout />);
-  });
-});
