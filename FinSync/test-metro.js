59f06a6570edc79858bdec3fc37e4f5399287bbf
Eduard Kakosyan <90533005+EduardKakosyan@users.noreply.github.com>
2025-07-10 09:09:08 -0300

    reset to clean state

--- FinSync/test-metro.js
+++
@@ -1,5 +0,0 @@
-// Simple test file to check Metro bundler
-console.log('Metro bundler test - this should serve as application/javascript');
-export default function TestComponent() {
-  return null;
-}
